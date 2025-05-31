f924f49b58a8eda397e7f47bd19fa9cbf3a5121d
ii02735 <yadallee.bilaal@outlook.fr>
2025-05-31 23:24:36 +0200

    init

---
+++ src/main/java/com/ii02735/learn_spring_security_6/LearnSpringSecurity6Application.java
@@ -0,0 +1,13 @@
+package com.ii02735.learn_spring_security_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringSecurity6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringSecurity6Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ii02735/learn_spring_security_6/LearnSpringSecurity6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ii02735.learn_spring_security_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringSecurity6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
